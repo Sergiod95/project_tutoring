d6e9885d011bf1ab0f433b79a80401bc2f98db83
Sergio David Gonzalez <sergiod.95@gmail.com>
2022-04-19 02:56:43 -0400

    Completing the foundations of the logins
    finished integrating the sign in and up pages. add the navbar base for future changes. added the session controls to keep track of the user in the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/", to: "main#index"
+
+  root to: "main#index"
   
   get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registration#create"
+  post "sign_up", to: "registrations#create"
+
 
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+  
 end
