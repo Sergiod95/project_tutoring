2f26ca8bbdeaaf2089ad6cf04938857e33679783
Sergio David Gonzalez <sergiod.95@gmail.com>
2022-05-26 10:23:11 -0400

    1.0.7

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -36,7 +36,7 @@ class AppointmentsController < ApplicationController
   end
 
   def calendarlist
-    @appointments = Appointment.where("DATE(start_time) == ? ", params[:date])
+    @appointments = Appointment.where("start_time.to_date == ? ", params[:date])
   end
 
 
