cc13ab3f3a76669aaf77488664a79438223b2339
Sergio David Gonzalez <sergiod.95@gmail.com>
2022-05-26 22:32:03 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
 	
+	validates :userid, presence: true
 	validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 
 	def name_with_lastname
